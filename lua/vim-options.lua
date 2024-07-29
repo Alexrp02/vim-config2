d1458a88ebdc1ee565814c81c25fa172e93faded
Alejandro Ramos <alejandro.ramos@constellaintelligence.com>
2024-07-29 12:30:46 +0200

    Replace plugin

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,11 +6,17 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 
+-- windows
+vim.keymap.set("n", "<leader>w", "<c-w>", { desc = "Windows", remap = true })
+vim.keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
+vim.keymap.set("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
+vim.keymap.set("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
+
 vim.keymap.set({ "n", "i" }, "<C-s>", function()
-	if vim.api.nvim_get_mode().mode == 'i' then
-		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<ESC>', true, false, true), 'n', true)
+	if vim.api.nvim_get_mode().mode == "i" then
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<ESC>", true, false, true), "n", true)
 	end
-	vim.cmd('w') -- Save the file
+	vim.cmd("w") -- Save the file
 	--	vim.lsp.buf.format({ async = true }) -- Format the code
 end, { silent = true })
 
