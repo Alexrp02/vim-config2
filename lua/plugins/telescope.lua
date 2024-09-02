f3de980e8230d85a2b1a7eaa4e1155b0ecfbaa53
Alejandro Ramos <alejandro.ramos@constellaintelligence.com>
2024-09-02 12:25:34 +0200

    Support for trouble and telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,27 +1,82 @@
 return {
 	{
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function ()
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader><leader>", builtin.find_files, {desc="Search files with telescope"})
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, {desc="Search with grep on folder"})
-		vim.keymap.set("n", "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", {desc="Grep search current buffer"})
-	end
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Search files with telescope" })
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Search with grep on folder" })
+			vim.keymap.set(
+				"n",
+				"<leader>sb",
+				"<cmd>Telescope current_buffer_fuzzy_find<cr>",
+				{ desc = "Grep search current buffer" }
+			)
+		end,
 	},
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
-		config = function ()
-			require("telescope").setup ({
-			  extensions = {
-			    ["ui-select"] = {
-			      require("telescope.themes").get_dropdown {
-			      }
-			    }
-			  }
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
 			})
 			require("telescope").load_extension("ui-select")
-		end
-	}
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		config = function()
+			local open_with_trouble = require("trouble.sources.telescope").open
+
+			local telescope = require("telescope")
+
+			telescope.setup({
+				defaults = {
+					mappings = {
+						i = { ["<c-t>"] = open_with_trouble },
+						n = { ["<c-t>"] = open_with_trouble },
+					},
+				},
+			})
+		end,
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
 }
