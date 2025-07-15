509733c36f6b4eb78a382bfb8afa2c2b7f0f15c6
AlejandroRP <alejandro.ramos@innovasur.com>
2025-07-15 14:57:48 +0200

    Add diffview

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"sindrets/diffview.nvim",
+		cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles" },
+		keys = {
+			{ "<leader>do", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+			{ "<leader>dc", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+			{ "<leader>dt", "<cmd>DiffviewToggleFiles<cr>", desc = "Toggle Diffview Files" },
+		},
+		config = function()
+			require("diffview").setup({
+				use_icons = true,
+			})
+		end,
+	}
+}
