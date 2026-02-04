920c2ddad3f063e76277628b956e4698906cea50
AlejandroRP <alejandro.ramos@innovasur.com>
2026-02-04 16:46:53 +0100

    feat: add bamboo colorscheme and set as default & move catpuccin to colorschemes file

--- lua/plugins/color-schemes.lua
+++ lua/plugins/color-schemes.lua
@@ -1,12 +1,30 @@
 return {
 	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			-- vim.cmd.colorscheme "catppuccin-mocha"
+		end,
+	},
+	{
 		"navarasu/onedark.nvim",
 		priority = 1000,
 		config = function()
-			require("onedark").setup({
-				style = "darker",
-			})
-			require("onedark").load()
+			-- require("onedark").setup({
+			-- 	style = "darker",
+			-- })
+			-- require("onedark").load()
+		end,
+	},
+	{
+		"ribru17/bamboo.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("bamboo").setup({})
+			require("bamboo").load()
 		end,
 	},
 }
