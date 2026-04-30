e327e8bf1b66e30894cb6dd760e80b123f09868f
AlexRP <aleeexrp02@gmail.com>
2026-04-30 20:20:28 +0200

    fix: update treesitter to use the correct main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,35 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		build = ":TSUpdate",
 		config = function()
-			local configs = require("nvim-treesitter.configs")
-			configs.setup({
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
+			-- Explicitly install parsers.
+			-- Prioritized Rust and TypeScript for your workflow, plus essential Neovim languages.
+			local parsers = {
+				"rust",
+				"typescript",
+				"tsx",
+				"lua",
+				"vim",
+				"vimdoc",
+				"markdown",
+				"markdown_inline",
+			}
+			require("nvim-treesitter").install(parsers)
+
+			-- Native Treesitter attachment replaces the old `highlight = { enable = true }`
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("treesitter_highlight", { clear = true }),
+				callback = function()
+					pcall(vim.treesitter.start)
+				end,
 			})
 		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
+		-- This plugin handles itself well and doesn't rely on the dead configs module.
 		config = function()
 			require("treesitter-context").setup()
 			vim.keymap.set("n", "gp", function()
@@ -25,30 +42,29 @@ return {
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main", -- YOU MUST KEEP THIS HERE
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		config = function()
-			local configs = require("nvim-treesitter.configs")
-			configs.setup({
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-						},
+			-- Setup textobjects directly using its own module
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
 					},
-					move = {
-						enable = true,
-						set_jumps = true,
-						goto_next_start = {
-							["]m"] = "@function.outer",
-						},
-						goto_next_end = {
-							["]M"] = "@function.outer",
-						},
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]m"] = "@function.outer",
+					},
+					goto_next_end = {
+						["]M"] = "@function.outer",
 					},
 				},
 			})
